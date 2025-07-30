7ee234f4c66f1342c37f14c46367d80e1d1b4532
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-07-30 13:30:34 +0800

    addingAttributesToStudent

--- app/src/main/java/studentmanager/Student.java
+++ app/src/main/java/studentmanager/Student.java
@@ -6,4 +6,68 @@ public class Student {
     private String firstName;
     private String lastName;
     private int age;
+    private String email;
+    private String course;
+
+    // Full constructor for Student object class
+    public Student(String studentId, String firstName, String lastName, int age, String email, String course) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.course = course;
+    }
+
+    // Getter methods for each attribute
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+
+    // Setter methods for each attribute
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
 }
