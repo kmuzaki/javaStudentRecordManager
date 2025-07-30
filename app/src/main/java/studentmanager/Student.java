9b918b1ff9ed69ce60b970832b88cddd255db301
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-07-30 13:20:45 +0800

    init

---
+++ app/src/main/java/studentmanager/Student.java
@@ -0,0 +1,9 @@
+package studentmanager;
+
+public class Student {
+    // Fields for student attributes
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private int age;
+}
